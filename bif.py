5a54fcd129b1429d9575cf40fc832d59ff802ce3
Jean-Marc Couffin <jmcouffin@users.noreply.github.com>
2025-01-09 15:34:28 +0100

    version

--- bif.py
+++ bif.py
@@ -25,6 +25,8 @@ from os import walk, path
 from sys import argv
 from chardet import detect
 
+VER = 1.*.*
+
 def get_rvt_file_version(rvt_file):
     if path.exists(rvt_file): 
         if isOleFile(rvt_file):
